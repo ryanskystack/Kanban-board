2e6ba9d62daac4225e4c623a55b5afd6e9771128
ryanskystack <sadleopard@163.com>
2021-10-09 23:16:04 +1100

    Updates

---
+++ precache-manifest.b615fb33e4caea51a0e812f3e1f0aa39.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea959d2297e41cea44092b64e1a2fe20",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "cc7d2517c0c628a199bc",
+    "url": "/kanban-board/static/js/2.abe55a6a.chunk.js"
+  },
+  {
+    "revision": "a635d881ce1eb5bc3f45",
+    "url": "/kanban-board/static/js/main.99d64d99.chunk.js"
+  },
+  {
+    "revision": "2a97cbc7210d9a44b96f",
+    "url": "/kanban-board/static/js/runtime~main.8507f43b.js"
+  }
+]);
